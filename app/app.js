a8478aebd2cbcfa5a4696b3aaafcbfc9ea2ce2df
Esterling Accime <esterling@brainbytescreative.com>
2017-10-07 18:15:49 -0400

    Router is now working

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 // Include the Main React Dependencies
-var React = require('react');
-var ReactDOM = require('react-dom');
-// Include the Main Component
-var routes = require("./components/Routes");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './components/Routes';
+
+// import App from './components/Test';
 // This code here allows us to render our main component (in this case "Main")
-ReactDOM.render(routes, document.getElementById("app"));
+ReactDOM.render(<Routes />, document.getElementById("app"));
